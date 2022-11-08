18f5f35aa72d9ca3828d0da1a02b7da20982bb39
WIKSATA1337 <ciganskigeigerwiksa26g@gmail.com>
2022-11-08 19:21:01 +0200

    Added exception handler

--- SudokuSolver/IO/IO.cs
+++ SudokuSolver/IO/IO.cs
@@ -4,17 +4,26 @@
     {
         public static void Start(Sudoku sudoku)
         {
-            Writer.NpcTalk("Sudoku Solver Started...", 2000, true);
+            try
+            {
+                Writer.NpcTalk("Sudoku Solver Started...", 2000, true);
 
-            Writer.NpcTalk("Sudoku Starting Pattern:", 1000);
-            Writer.SudokuPrint(sudoku, 0);
-            Writer.ButtonAwaiter(true);
+                Writer.NpcTalk("Sudoku Starting Pattern:", 1000);
+                Writer.SudokuPrint(sudoku, 0);
+                Writer.ButtonAwaiter(true);
 
-            sudoku.Solve();
-            Writer.NpcTalk("Solving...", 2000, true);
+                sudoku.Solve();
+                Writer.NpcTalk("Solving...", 2000, true);
 
-            Writer.NpcTalk("Result:", 500);
-            Writer.SudokuPrint(sudoku, 0);
+                Writer.NpcTalk("Result:", 500);
+                Writer.SudokuPrint(sudoku, 0);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sorry, an error occured while trying to solve the Sudoku.");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
